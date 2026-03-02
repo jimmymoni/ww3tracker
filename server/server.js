6ef537605ce785d50cd16b8c3ed1aeb8b64512c1
jimmy davis <jimmydavis00@gmail.com>
2026-03-03 00:41:38 +0530

    Add SEO: meta description, canonical, fix OG/Twitter URLs, add Schema JSON-LD, add robots.txt with AI bot access

--- server/server.js
+++ server/server.js
@@ -677,6 +677,11 @@ app.get('/sitemap.xml', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/sitemap.xml'));
 });
 
+// Explicit route for robots.txt (before SPA catch-all)
+app.get('/robots.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/robots.txt'));
+});
+
 // Serve index.html for all other routes (SPA routing)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
